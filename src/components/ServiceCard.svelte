e5dcda553c4e1a60a3e90951d3b8d9a1fe99dbfc
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-04-17 16:36:57 +0330

    Services page ...

---
+++ src/components/ServiceCard.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import clsx from "clsx";
+    export let title: string;
+    let className: string = '';
+    export { className as class };
+</script>
+
+<div class={clsx("sv-card", className)}>
+    <h2 class="sv-title">{title}</h2>
+
+    <p class="sv-desc">
+        <slot />
+    </p>
+</div>
+
+<style lang="scss">
+    .sv {
+        &-card {
+            @apply bg-slate-100 rounded-xl px-4 py-4 w-full;
+        }
+
+        &-title {
+            @apply text-lg font-semibold text-center;
+        }
+
+        &-desc {
+            @apply text-sm mt-3;
+        }
+    }
+
+    :global(.dark) {
+        .sv-card {
+            @apply bg-slate-800 shadow-dark;
+        }
+    }
+</style>
---
+++ src/pages/services.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import ServiceCard from "@/components/ServiceCard.svelte";
+</script>
+
+<div class="row">
+    <div class="item">
+        <ServiceCard title={$_("front-end.label")}>
+            {$_("front-end.description")}
+        </ServiceCard>
+    </div>
+
+    <div class="item">
+        <ServiceCard title={$_("back-end.label")}>
+            {$_("back-end.description")}
+        </ServiceCard>
+    </div>
+
+    <div class="item">
+        <ServiceCard title={$_("telegram-bot.label")}>
+            {$_("telegram-bot.description")}
+        </ServiceCard>
+    </div>
+</div>
+
+<style lang="scss">
+    .row {
+        // @apply grid grid-cols-2 gap-6;
+        @apply flex flex-wrap w-full -mx-3;
+        .item {
+            @apply max-w-full w-4/12 flex-auto px-6 h-fit;
+            @apply flex flex-col gap-10;
+        }
+    }
+</style>
