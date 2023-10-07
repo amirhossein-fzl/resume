bede9c86fe397826f39c9b1abfe41bf21547af58
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-07 17:09:41 +0330

    Add navigation links ...

--- src/components/sections/Navigation.vue
+++ src/components/sections/Navigation.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
-    import LanguageSwitcher from './LanguageSwitcher.vue';
+import LanguageSwitcher from './LanguageSwitcher.vue';
 </script>
 
 <template>
     <nav>
         <LanguageSwitcher />
+
+        <ul class="links">
+            <li>
+                <a href="/">{{ $t("about_me") }}</a>
+            </li>
+            <li>
+                <a href="#/skills">{{ $t("my_skills") }}</a>
+            </li>
+            <li>
+                <a href="#/services">{{ $t("my_services") }}</a>
+            </li>
+            <li>
+                <a href="#/portfolio">{{ $t("my_portfolios") }}</a>
+            </li>
+            <li>
+                <a href="#/contact">{{ $t("contact_me") }}</a>
+            </li>
+        </ul>
     </nav>
 </template>
 
 <style scoped lang="scss">
-    nav {
-        @apply bg-white shadow-light p-4 w-full rounded-xl;
-        @apply flex justify-between items-center;
-    }
+nav {
+    @apply bg-white shadow-light p-4 w-full rounded-xl;
+    @apply flex justify-between items-center;
+}
+
+.links {
+    @apply flex gap-4
+}
 </style>
