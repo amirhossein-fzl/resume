bb345b615466479085089379b2c8eaf543e18b2d
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-12 21:48:28 +0330

    WithProfileLayout component ...

---
+++ src/layouts/WithProfileLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Profile from '@/components/sections/Profile.vue';
+</script>
+
+<template>
+    <div class="ct">
+        <div class="item">
+            <Profile />
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+// Container
+.ct {
+    @apply flex flex-col gap-20;
+}
+.item {
+    @apply w-6/12 mx-auto;
+}
+</style>
