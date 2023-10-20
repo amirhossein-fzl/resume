0d39232c3a4023d985e1e7d366021c06adc4ede7
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-20 16:28:03 +0330

    Add animation for router change and peofile ...

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -8,7 +8,11 @@ import { RouterView } from "vue-router";
         <Navigation />
         
         <div class="content">
-            <RouterView />
+            <RouterView v-slot="{ Component }">
+                <Transition name="page">
+                    <component :is="Component" />
+                </Transition>
+            </RouterView>
         </div>
     </div>
 </template>
@@ -21,5 +25,21 @@ import { RouterView } from "vue-router";
 
 .content {
     @apply mt-20;
+}
+
+.page {
+    &-enter-active,
+    &-leave-active {
+        @apply duration-300;
+    }
+
+    &-enter-to {
+        @apply scale-100;
+    }
+
+    &-enter-from,
+    &-leave-to {
+        @apply scale-75 opacity-0 h-0;
+    }
 }
 </style>
--- src/layouts/WithProfileLayout.vue
+++ src/layouts/WithProfileLayout.vue
@@ -5,7 +5,9 @@ import Profile from '@/components/sections/Profile.vue';
 <template>
     <div class="ct">
         <div class="item">
-            <Profile />
+            <Transition name="scale" appear>
+                <Profile />
+            </Transition>
         </div>
         <div>
             <slot />
@@ -20,5 +22,19 @@ import Profile from '@/components/sections/Profile.vue';
 }
 .item {
     @apply w-10/12 sm:w-8/12 mx-auto;
+}
+
+.scale {
+    &-enter-active, &-leave-active {
+        @apply duration-300 delay-300;
+    }
+
+    &-enter-to {
+        @apply scale-100;
+    }
+
+    &-enter-from, &-leave-to {
+        @apply opacity-0 scale-90;
+    }
 }
 </style>
