a85660c7b8c2f96d38ac0a302b468ab31d970145
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-20 18:09:35 +0330

    Add @unhead/vue package for complete pages seo meta tags ...

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./routes";
 import i18n from "./plugins/i18n";
+import { createHead } from "@unhead/vue";
 
 const pinia = createPinia();
+const head = createHead();
 
 createApp(App)
 .use(pinia)
+.use(head)
 .use(router)
 .use(i18n)
 .mount("#app");
