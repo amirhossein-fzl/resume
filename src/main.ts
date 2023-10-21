ac84c5906bd3148560ba65b1c0b78484a101569e
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-21 14:00:44 +0330

    Add SSR Support ...

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
-import { createApp } from "vue";
-import "@/assets/styles/app.scss";
-import App from "./App.vue";
-import { createPinia } from "pinia";
-import router from "./routes";
-import i18n from "./plugins/i18n";
-import { createHead } from "@unhead/vue";
+import { createSSRApp } from 'vue';
+import '@/assets/styles/app.scss';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+import router from './routes';
+import i18n from './plugins/i18n';
+import { createHead } from '@unhead/vue';
 
-const pinia = createPinia();
-const head = createHead();
+export const createApp = () => {
+    const pinia = createPinia();
+    const head = createHead();
 
-createApp(App)
-.use(pinia)
-.use(head)
-.use(router)
-.use(i18n)
-.mount("#app");
+    const app = createSSRApp(App);
+    app.use(pinia);
+    app.use(head);
+    app.use(router);
+    app.use(i18n);
+    return {
+        app,
+        router,
+        head,
+    };
+};
