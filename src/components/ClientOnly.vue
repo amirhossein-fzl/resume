fc5228d29a9f5d562567f79b11f67789e1a99b4b
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-21 14:01:46 +0330

    Add ClientOnly component for render only client side ...

---
+++ src/components/ClientOnly.vue
@@ -0,0 +1,18 @@
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+const show = ref(false);
+
+onMounted(() => {
+    show.value = true;
+});
+
+</script>
+
+<template>
+    <slot v-if="show" />
+</template>
+
+<style scoped lang="scss">
+    
+</style>
