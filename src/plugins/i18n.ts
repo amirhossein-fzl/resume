7a0c6a0e087da5f9420010abddded689600b7059
Amirhossein Fazli <amirhossein95b@gmail.com>
2023-10-21 13:04:11 +0330

    Add ssr default language in i18n config ...

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,10 +1,11 @@
 import { createI18n } from "vue-i18n";
 import en from "@/assets/langs/en.json";
 import fa from "@/assets/langs/fa.json";
+import config from "@config";
 
 export default createI18n({
     legacy: false,
-    locale: "en",
+    locale: config.lang_in_ssr,
     messages: {
         en,
         fa,
